b06aea7f8e9e310b49e7129c306e96d750de5d8c
3lLobo <flocwolf@gmail.com>
2021-12-24 20:42:21 +0100

    Feliz navidad :sparkles:

--- 2021/navidad/src/main.rs
+++ 2021/navidad/src/main.rs
@@ -1,3 +1,80 @@
+use std::fs;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, yellow Submarine!");
+
+    let alu = ALU {w: 0, x: 0, y: 0, z: 0};
+
+    println!("{:#}", &alu.x);
+    
+    let data = read_data();
+
+    for line in data.lines() {
+
+        let cmds = line.clone().split(" ");
+
+        for cmd in cmds {
+            println!("{:#?}", &cmd);
+        }
+
+        // match cmds.next() {
+        //     std::option::Option::from("imp") => alu.imp(cmds.next()),
+        //     "add" => alu.add(cmds.next(), cmds.next()),
+        // }
+    }
+
+}
+
+
+pub fn read_data() -> String {
+    let data = fs::read_to_string("../input_data/input_navidad.txt").unwrap_or_else(|_| panic!("Hulp"));
+    data
+}
+
+
+struct ALU {
+    w: i32,
+    x: i32,
+    y: i32,
+    z: i32
+}
+
+
+impl ALU {
+
+    fn imp(&self, var1: char) -> i32 {
+
+        if var1 == 'w' {
+            return self.w;
+        } else if var1 == 'x' {
+            return self.x;
+        } else if var1 == 'y' {
+            return self.y;
+        } else if var1 == 'z' {
+            return self.z;
+        } else {
+            return self.w;
+        }
+    }
+
+    fn add(var1: char, var2: char) {
+        return
+    }
+
+    fn mul(var1: char, var2: char) {
+        return
+    }
+
+    fn mud(var1: char, var2: char) {
+        return
+    }
+
+    fn div(var1: char, var2: char) {
+        return
+    }
+
+    fn eql(var1: char, var2: char) {
+        return
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# advent_of_code
+# Advent of code :sparkles:
+
+[![CI](https://github.com/3lLobo/advent_of_code/actions/workflows/rust.yml/badge.svg)](https://github.com/3lLobo/advent_of_code/actions/workflows/rust.yml) ![GitHub last commit](https://img.shields.io/github/last-commit/3lLobo/advent_of_code) ![Lines of code](https://img.shields.io/tokei/lines/github/3lLobo/advent_of_code)
+
+
 Solutions to the [advent of code](https://adventofcode.com/ "Click meee!!!")  challenge.
