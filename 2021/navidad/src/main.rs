31fea3c330636dfe7507a547a80bc6e6b23876a6
3lLobo <flocwolf@gmail.com>
2021-12-25 23:57:44 +0100

    Matching

--- 2021/navidad/src/main.rs
+++ 2021/navidad/src/main.rs
@@ -1,27 +1,34 @@
 use std::fs;
+use std::fmt;
 
 
 fn main() {
     println!("Hello, yellow Submarine!");
 
-    let alu = ALU {w: 0, x: 0, y: 0, z: 0};
+    let alu = ALU {w: 0, x: 0, y: 0, z: 0, secret_code: Vec::new()};
 
-    println!("{:#}", &alu.x);
+    // println!("{:#}", &alu.x);
     
     let data = read_data();
 
     for line in data.lines() {
 
-        let cmds = line.clone().split(" ");
+        // let cmds = line.split(" ")
+        //                 .map(|x| x.parse::<str>().unwrap())
+        //                 .collect();
+
+        let cmds: Vec<String> = line.clone().split(" ").map(|x| x.parse().unwrap()).collect();
+
+        dbg!(&cmds);
 
         for cmd in cmds {
             println!("{:#?}", &cmd);
         }
 
         // match cmds.next() {
-        //     std::option::Option::from("imp") => alu.imp(cmds.next()),
-        //     "add" => alu.add(cmds.next(), cmds.next()),
-        // }
+        //     Some("imp") => alu.imp(&cmds.next().unwrap()),
+        //     Some("add") => alu.add(&cmds.next().unwrap(), &cmds.next().unwrap()),
+        // };
     }
 
 }
@@ -33,48 +40,83 @@ pub fn read_data() -> String {
 }
 
 
+enum AluCmd {
+    Imp {x: String},
+    Add {x: String, y: String},
+    Mul {x: String, y: String},
+    Div {x: String, y: String},
+    Mud {x: String, y: String},
+    Eql {x: String, y: String},
+}
+
+
+#[derive(Debug)]
 struct ALU {
     w: i32,
     x: i32,
     y: i32,
-    z: i32
+    z: i32,
+    secret_code: Vec<i32>,
 }
 
 
 impl ALU {
 
-    fn imp(&self, var1: char) -> i32 {
+    fn match_var(&self, var1: &str) -> i32 {
 
-        if var1 == 'w' {
+        match var1 {
+            "w" => self.w,
+            "x" => self.x,
+            x => x.parse().unwrap(),
+        }
+    }
+
+    fn match_cmd(&mut self, opt_cmd: AluCmd) {
+
+        match opt_cmd {
+            AluCmd::Imp {x} => self.secret_code.push(x.parse().unwrap()),
+            AluCmd::Add { x, y } => (),
+            AluCmd::Mul { x, y } => (),
+            AluCmd::Div { x, y } => (),
+            AluCmd::Mud { x, y } => (),
+            AluCmd::Eql { x, y } => (),
+        }
+    }
+
+    fn imp(&self, var1: &str) -> i32 {
+
+        if var1 == "w" {
             return self.w;
-        } else if var1 == 'x' {
+        } else if var1 == "x" {
             return self.x;
-        } else if var1 == 'y' {
+        } else if var1 == "y" {
             return self.y;
-        } else if var1 == 'z' {
+        } else if var1 == "z" {
             return self.z;
         } else {
             return self.w;
         }
     }
 
-    fn add(var1: char, var2: char) {
+    fn add(&self, var1: &str, var2: &str) -> i32 {
+        let var1 = var1;
+        println!("{} + {}", &var1, &var2);
+        return var1.parse::<i32>().unwrap();
+    }
+
+    fn mul(&self, var1: &str, var2: &str) {
         return
     }
 
-    fn mul(var1: char, var2: char) {
+    fn mud(&self, var1: &str, var2: &str) {
         return
     }
 
-    fn mud(var1: char, var2: char) {
+    fn div(&self, var1: &str, var2: &str) {
         return
     }
 
-    fn div(var1: char, var2: char) {
-        return
-    }
-
-    fn eql(var1: char, var2: char) {
+    fn eql(&self, var1: &str, var2: &str) {
         return
     }
 }
